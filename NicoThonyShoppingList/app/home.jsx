addcd0a6d5ef01aa141a766d38f6f5666ba342d9
AnthonyRodrigues894 <anthonyserrano894@gmail.com>
2025-05-12 11:39:06 -0300

    continuaEstilizando

--- NicoThonyShoppingList/app/home.jsx
+++ NicoThonyShoppingList/app/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
@@ -18,7 +18,17 @@ export default function Home() {
         <View style={{ flex: 1}}></View>
 
         <View style={styles.footer}>
-          
+          <View style={styles.imputcontainer}>
+            <TextInput 
+              color="#fff"
+              fontSize={18}
+              placeholderTextColor={"#aeaeae"}
+              placeholder='Digite o nome do produto...'
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name="add" size={36} color="#fff" />
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </SafeAreaView>
@@ -40,5 +50,33 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     color: '#fff'
+  },
+  footer: {
+    backgroundColor: '#000000c0',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  imputcontainer: {
+    flex: 1,
+    backgroundColor: "#000",
+    height: 50,
+    marginVertical: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#000',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
